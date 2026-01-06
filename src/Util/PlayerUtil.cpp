6d7cc57821105e6c488a1bd0a3e3510b6e974c6e
emvivre <emvivre@urdn.com.ua>
2026-01-06 18:26:28 +0100

    Add implementation of `bool rs::isNearPlayerH(const al::LiveActor* actor, f32 threshold)`

--- src/Util/PlayerUtil.cpp
+++ src/Util/PlayerUtil.cpp
@@ -1,7 +1,16 @@
 #include "Util/PlayerUtil.h"
 
 #include "Library/LiveActor/ActorPoseUtil.h"
+#include "Library/Math/MathUtil.h"
 #include "Library/Player/PlayerUtil.h"
+
+bool rs::isNearPlayerH(const al::LiveActor* actor, f32 threshold) {
+    const al::LiveActor* player = al::getPlayerActor(actor, 0);
+    sead::Vector3f diff = al::getTrans(player) - al::getTrans(actor);
+    const sead::Vector3f& gravity = al::getGravity(actor);
+    al::verticalizeVec(&diff, gravity, diff);
+    return diff.length() < threshold;
+}
 
 const sead::Vector3f& rs::getPlayerPos(const al::LiveActor* actor) {
     const al::LiveActor* player = al::getPlayerActor(actor, 0);
